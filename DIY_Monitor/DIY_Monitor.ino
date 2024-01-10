bc2f397b195c94fb7f3f00aaa74617994cd9ea97
Yuhua (Annie）Jin <146268411+ucfninf@users.noreply.github.com>
2024-01-10 00:28:17 +0000

    Add files via upload

--- DIY_Monitor/DIY_Monitor.ino
+++ DIY_Monitor/DIY_Monitor.ino
@@ -1,16 +1,16 @@
-#include <DHT.h>                //输入
+#include <DHT.h>                //input
 #include <DHT_U.h>
-#include <Adafruit_NeoPixel.h>  //输出
-#include <FastLED.h> //插件？
+#include <Adafruit_NeoPixel.h>  //Output
+#include <FastLED.h> //extension
 
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-#define MAX4466_PIN A0  // 噪音传感器连接的模拟引脚
-#define LDR_PIN A1      // 光敏电阻连接的模拟引脚
+#define MAX4466_PIN A0  // Analog pin for noise sensor connection
+#define LDR_PIN A1      // Analog pin for photoresistor connection
 
-#define LED_PIN 4   // LED 灯条连接的数字引脚
-#define NUM_LEDS 8  // LED 灯条上的LED数量
+#define LED_PIN 4   // Digital pins for LED strip connections
+#define NUM_LEDS 8  // The number of LEDs on the LED strip
 
-#define Vcc 5.0  // 板子电压
+#define Vcc 5.0  // Board Voltage
 
 int color1 = 0;
 int color2 = 0;
@@ -26,9 +26,9 @@ const int sampleWindow = 50;  // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
 int brightness = 128;  // Initialize brightness to a default value
 
-// 零点校准值和灵敏度校准值，根据传感器规格调整 *********************************
-float sensitivityCalibration = 5.0;  // 传感器灵敏度
-int zeroPointCalibration = 100;  // 传感器零点校准值
+// Zero point calibration value and sensitivity calibration value, adjusted according to sensor specifications *********************************
+float sensitivityCalibration = 5.0;  // Sensor sensitivity
+int zeroPointCalibration = 100;  // Sensor zero point calibration value
 
 
 void setup() {
@@ -43,7 +43,7 @@ void setup() {
   pinMode(DHTPin, INPUT);
   dht.begin();
 
-  // Initialize FastLED 新加的内容
+  // Initialize FastLED 
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
   FastLED.show(); // Initialize all LEDs to "off"
 }
@@ -51,11 +51,11 @@ void setup() {
 
 void loop() {
 
-  // 读取温湿度数据
+  // Read temperature and humidity data
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
 
-  // 读取噪音水平
+  // Reading noise level
   int noiseLevel = analogRead(MAX4466_PIN);
   unsigned long startMillis = millis(); // Start of sample window
   unsigned int peakToPeak = 0;   // peak-to-peak level
@@ -63,7 +63,7 @@ void loop() {
   unsigned int signalMax = 0;
   unsigned int signalMin = 1024;
 
-// 转换为电压********************************************************
+// Convert to voltage********************************************************
 float volts = (noiseLevel * 5.0) / 1024.0;
 
   // collect data for 50 mS
@@ -87,7 +87,7 @@ float volts = (noiseLevel * 5.0) / 1024.0;
   // double volts = (peakToPeak * 5.0) / 256;  // convert to volts************************
   Serial.println(volts);
 
- // 将传感器读数转换为分贝********************************
+ // Convert sensor readings to decibels********************************
   float dB = 20 * log10(volts / sensitivityCalibration) + zeroPointCalibration;
   Serial.println(noiseLevel);
 
@@ -106,67 +106,67 @@ float volts = (noiseLevel * 5.0) / 1024.0;
   Serial.print(" LDR Value: ");
   Serial.println(lightIntensity);
 
-  delay(200);  // 等待一段时间再进行下一次读取
+  delay(100);  // Wait for a while before doing the next read
 
-// 在LED条上显示光照强度
+// Display light intensity on LED strip
   int mappedIntensity = map(lightIntensity, 0, 1023, 0, 255);
   for (int i = 0; i < NUM_LEDS; i++) {
     strip.setPixelColor(i, strip.Color(mappedIntensity, mappedIntensity, 0));
   }
 
 
-  // 设置 LED 条的亮度
+  // Set the brightness of the LED strip
   for (int i = 5; i < NUM_LEDS; i++) {
-    leds[i].nscale8(brightness);  // 根据亮度进行渐变
+    leds[i].nscale8(brightness);  // Gradient based on brightness
   }
    
-  // 根据噪音水平控制灯条颜色
-  if (noiseLevel > 700) {
+  // Control light strip color based on noise level
+  if (noiseLevel > 600) {
     for (int i = 1; i < 7 ; i++) {
-      strip.setPixelColor(i, strip.Color(255, 0, 0));  // 红色
-    } 
+      strip.setPixelColor(i, strip.Color(255, 0, 0));  // Red
+    } //Above 500, red light lights up
   }
   
-  else if (noiseLevel > 300 && noiseLevel < 700){
+  else if (noiseLevel > 300 && noiseLevel < 600){
     for (int i = 1; i < 7 ; i++) {
-      strip.setPixelColor(i, strip.Color(87, 25, 0));  // 黄色
+      strip.setPixelColor(i, strip.Color(87, 25, 0));  // Yellow
     } 
-  // 500以下，数值中间显示黄色光
+  // Below 500, yellow light appears in the middle of the value
   }
 
   else {
     for (int i = 1; i < 7 ; i++) {
-      strip.setPixelColor(i, strip.Color(87, 70, 89));  // 白蓝色
+      strip.setPixelColor(i, strip.Color(87, 70, 89));  // light blue
     } 
-  // 500以下，最小显示蓝色光
+  // Below 500, minimum display blue light
   }
 
   if (temperature > 26) {
-      strip.setPixelColor(0, strip.Color(255, 0, 0));  // 红色
+      strip.setPixelColor(0, strip.Color(255, 0, 0));  // red
   }
   
-  else if (temperature > 22 && temperature < 26){
-      strip.setPixelColor(0, strip.Color(87, 25, 0));  // 黄色
-  // 500以下，数值中间显示黄色光
+  else if (temperature > 21 && temperature < 26){
+      strip.setPixelColor(0, strip.Color(87, 25, 0));  // yellow
+  // Below 500, yellow light appears in the middle of the value
   }
 
   else {
-      strip.setPixelColor(0, strip.Color(87, 70, 89));  // 白蓝色
-  // 500以下，最小显示蓝色光
+      strip.setPixelColor(0, strip.Color(87, 70, 89));  // light blue
+  // Below 500, minimum display blue light
   }
 
-  if (humidity > 45) {
-      strip.setPixelColor(7, strip.Color(255, 0, 0));  // 红色
+  if (humidity < 30) {
+      strip.setPixelColor(7, strip.Color(255, 0, 0));  // Red
   }
   
-  else if (humidity > 34 && humidity < 45){
-      strip.setPixelColor(7, strip.Color(87, 25, 0));  // 黄色
-  // 500以下，数值中间显示黄色光
+  else if (humidity > 30 && humidity < 60){
+      strip.setPixelColor(7, strip.Color(87, 25, 0));  // Yellow
+  // Below 500, yellow light appears in the middle of the value
   }
 
   else {
-      strip.setPixelColor(7, strip.Color(87, 70, 89));  // 白蓝色
-  // 500以下，最小显示蓝色光
+      strip.setPixelColor(7, strip.Color(87, 70, 89));  // blue
+  // Below 500, minimum display blue light
   }
 
   strip.show(); // Show Adafruit_NeoPixel LED colors
